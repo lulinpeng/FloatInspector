01e8471e9014ee50e3256aa7b42d270aa0a8933c
Linpeng Lu <turingcrypt@gmail.com>
2025-04-11 17:53:22 +0800

    add more functions

--- floatinspector.py
+++ floatinspector.py
@@ -5,6 +5,7 @@ import math
 BYTE_BIT_LEN = 8
 FLOAT64_MAGNITUDE_BIT_LEN = 11
 FLOAT64_MANTISSA_BIT_LEN = 52
+FLOAT64_MAGNITUDE_OFFSET = (1 << (FLOAT64_MAGNITUDE_BIT_LEN -1)) - 1
 
 class FloatInspector:
     @staticmethod
@@ -42,21 +43,23 @@ class FloatInspector:
     
     @staticmethod
     def float64_to_bin(f:float):
-        ''' return the binary representation of big, IEEE 754, of a float64 number'''
-        import struct
-        packed = struct.pack('>d', f)  # '>d' for big-endian byte order
-        unpacked = struct.unpack('>q', packed)[0]
-        return f'{unpacked:064b}'
+        '''return the binary representation of big, IEEE 754, of a float64 number'''
+        # float -> bytes
+        packed_bytes = struct.pack('>d', f)  # '>' for big-endian byte order, # https://docs.python.org/3/library/struct.html
+        # bytes -> integer
+        a = int.from_bytes(packed_bytes, byteorder='big')
+        # integer -> bit string
+        return bin(a)[2:].zfill(64)
 
     @staticmethod
     def get_float64_magnitude_bin(f:float):
-        ''' return the magnitude part of a float64 number '''
+        '''return the magnitude part of a float64 number '''
         bin_str = FloatInspector.float64_to_bin(f)
         return bin_str[1:1+FLOAT64_MAGNITUDE_BIT_LEN]
     
     @staticmethod
     def get_float64_mantissa_bin(f:float):
-        ''' return the binary representation of mantissa part of a float64 number '''
+        '''return the binary representation of mantissa part of a float64 number '''
         bin_str = FloatInspector.float64_to_bin(f)
         return bin_str[1+FLOAT64_MAGNITUDE_BIT_LEN:]
     
@@ -66,37 +69,58 @@ class FloatInspector:
         return '1' if f < 0 else '0'
 
     @staticmethod
-    def get_float64_magnitude(f:float):
-        ''' return the magnitude part of a float64 number '''
+    def get_float64_magnitude_int(f:float):
+        '''return the magnitude part of a float64 number '''
         magnitude_bin = FloatInspector.get_float64_magnitude_bin(f)
-        magnitude = int(magnitude_bin, 2) - (1<<FLOAT64_MAGNITUDE_BIT_LEN - 1)
+        magnitude = int(magnitude_bin, 2)
         return magnitude
     
     @staticmethod
+    def get_float64_exponent(f:float):
+        '''return the binary exponent of a float64 number '''
+        return int(FloatInspector.get_float64_magnitude_bin(f), 2) - FLOAT64_MAGNITUDE_OFFSET
+    
+    @staticmethod
     def get_float64_mantissa(f:float):
-        ''' return the mantissa part of a float64 number '''
+        '''return the mantissa part of a float64 number '''
         mantissa_bin = '1' + FloatInspector.get_float64_mantissa_bin(f)
         mantissa = int(mantissa_bin, 2)
         return mantissa
     
     @staticmethod
     def get_float64_sign(f:float):
-        ''' return the sign bit of a float64 number '''
+        '''return the sign bit of a float64 number '''
         return 1 if f < 0 else 0
     
     @staticmethod
-    def pretty_print(f:float):
-        return f'({FloatInspector.get_float64_sign(f)}, {FloatInspector.get_float64_magnitude_bin(f)}, {FloatInspector.get_float64_mantissa_bin(f)})'
+    def split_print(f:float):
+        '''return sign/magnitude/mantissa in bit-form seperately'''
+        return f'{FloatInspector.get_float64_sign(f)}, {FloatInspector.get_float64_magnitude_bin(f)}, {FloatInspector.get_float64_mantissa_bin(f)}'
+
+    @staticmethod
+    def scientific_notation_print(f:float):
+        '''return the representation in scientific notation of a float64 number'''
+        sign = -1 if FloatInspector.get_float64_sign(f) else 1
+        mantissa = FloatInspector.get_float64_mantissa_bin(f).rstrip('0')
+        exponent = FloatInspector.get_float64_exponent(f)
+        return f"{sign}.{mantissa}" if exponent == 0 else f"{sign}.{mantissa} x 2^{{{exponent}}}"
 
 print(f'random number: {FloatInspector.random_n_bit_number(64)}')
 
-print(f'binary representation: {FloatInspector.float64_to_bin(1.1)}')
+f = -0.001
 
-print(f'sign bits: {FloatInspector.get_float64_sign_bin(1.1)}')
+print(f'binary representation: {FloatInspector.float64_to_bin(f)}')
 
-print(f'magnitude bits: {FloatInspector.get_float64_magnitude_bin(1.1)}')
+print(f'sign bits: {FloatInspector.get_float64_sign_bin(f)}')
 
-print(f'mantissa bits: {FloatInspector.get_float64_mantissa_bin(1.1)}')
+print(f'magnitude bits: {FloatInspector.get_float64_magnitude_bin(f)}')
 
-print(f'pretty_print: {FloatInspector.pretty_print(1.1)}')
+print(f'mantissa bits: {FloatInspector.get_float64_mantissa_bin(f)}')
+
+print(f'split_print: {FloatInspector.split_print(f)}')
+
+print(f'scientific_print: {FloatInspector.scientific_notation_print(f)}')
+
+
+
 
